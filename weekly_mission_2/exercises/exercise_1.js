9c7b060696fc83a029a1ba004e1b9bba29db085f
Alfredo Bonell <alfredobonell96@gmail.com>
2022-04-14 18:11:50 -0600

    Avances semana 2.
    - Se suben avances de los ejercicios 1 y 2.

--- weekly_mission_2/exercises/exercise_1.js
+++ weekly_mission_2/exercises/exercise_1.js
@@ -1,3 +1,4 @@
+// GitHub
 const repo = {
   name: "LaunchX",
   author: "AlfredoBonell",
@@ -14,7 +15,7 @@ const repo = {
     return `This repository ${this.name} was created by ${this.author}`;
   },
 };
-
+console.log(":::::::::::::::: GitHub - repo ::::::::::::::::");
 console.log("Nombre del repo:" + repo.name);
 console.log("Issues totales: " + repo.getTotalIssues());
 console.log(repo.getGeneralInfo());
@@ -37,6 +38,7 @@ const issue = {
   },
 };
 
+console.log(":::::::::::::::: GitHub - issue ::::::::::::::::");
 console.log("El titulo del nuevo issue es: " + issue.title);
 console.log("Número de comentarios: " + issue.numberOfComments);
 console.log(issue.getTitleAndAuthor());
@@ -57,7 +59,140 @@ const pullrequest = {
   },
 };
 
+console.log(":::::::::::::::: GitHub - pullrequest ::::::::::::::::");
 console.log("El titulo del nuevo pull request es: " + pullrequest.title);
 console.log("Nombre de la rama: " + pullrequest.branchName);
 console.log(pullrequest.getStatus());
 console.log(pullrequest.getTitleAndAuthor());
+
+// Twitter
+const user = {
+  user: "Alfredo",
+  username: "AlfredoBonell",
+  bio: "Lomitos son lo máximo",
+  age: 23,
+  getBio: function () {
+    return this.bio;
+  },
+};
+
+console.log(":::::::::::::::: Twitter - user ::::::::::::::::");
+console.log("Mi usuario es " + user.user);
+console.log("Tengo: " + user.age + " años");
+console.log(user.getBio());
+
+const trending_topic = {
+  username: "elonmusk",
+  top: [
+    "Breaking News: Elon Musk offered to buy Twitter",
+    "Elon Musk just made a $43 billion offer to buy 100% of Twitter",
+  ],
+  latest: [
+    "Elon Musk lanza una oferta de 40.000 millones de euros por Twitter.",
+    "Elon Musk says he ‘doesn’t care about the economics’ of buying Twitter – TechCrunch",
+  ],
+  getTop: function () {
+    return this.top;
+  },
+  getLatest: function () {
+    return this.latest;
+  },
+};
+
+console.log(":::::::::::::::: Twitter - trending_topic ::::::::::::::::");
+console.log("Usuario en tendencias " + trending_topic.username);
+console.log(trending_topic.getTop());
+console.log(trending_topic.getLatest());
+
+const hashtag = {
+  hashtag: "#ElonMusk, #JuevesSanto, #SemanaSanta2022",
+  getHashTag: function () {
+    return this.hashtag;
+  },
+};
+
+console.log(":::::::::::::::: Twitter - hashtag ::::::::::::::::");
+console.log(hashtag.getHashTag());
+
+// Facebook
+const userFb = {
+  user: "Alfredo",
+  username: "AlfredoBonell",
+  bio: "Lomitos son lo máximo",
+  age: 23,
+  friends: 500,
+  groups: 5,
+  getBio: function () {
+    return this.bio;
+  },
+};
+
+console.log(":::::::::::::::: Facebook - userFb ::::::::::::::::");
+console.log("Mi usuario es " + userFb.user);
+console.log("Tengo: " + userFb.friends + " amigos en Facebook");
+console.log("Estoy: " + userFb.groups + " grupos en Facebook sobre tecnología");
+console.log(userFb.getBio());
+
+const post = {
+  name: "Lo que he aprendido en LaunchX",
+  author: "Alfredo Bonell",
+  title: "JavaScript",
+  date: "2022-04-14",
+  likes: 4,
+  loves: 2,
+  getLikesAndLoves: function () {
+    return (
+      "Y tuvo " + this.likes + " me gustas y " + this.loves + " me encanta"
+    );
+  },
+};
+
+console.log(":::::::::::::::: Facebook - post ::::::::::::::::");
+console.log(
+  "La última publicación que hizo " + post.author + " fue sobre " + post.name
+);
+console.log(post.getLikesAndLoves());
+
+const biography = {
+  name: "Alfredo",
+  friends: 500,
+  post: 200,
+  age: 26,
+  country: "México",
+  photos: 60,
+};
+
+console.log(":::::::::::::::: Facebook - biography ::::::::::::::::");
+console.log("La biografía es de: " + biography.name);
+console.log("Vive en: " + biography.country);
+console.log("Y tiene: " + biography.photos);
+
+// Uber
+const profile = {
+  messages: 10,
+  username: "Alfredo Bonell",
+  ranking: 4.89,
+  travels: 900,
+  getTraverls: function () {
+    return "Y ha hecho " + this.travels + " viajes";
+  },
+  getRanking: function () {
+    return this.ranking;
+  },
+};
+
+console.log(":::::::::::::::: Uber - profile ::::::::::::::::");
+console.log("El perfil de Uber es: " + profile.username);
+console.log(
+  "Y tiene una calificación como usuario de: " + profile.getRanking()
+);
+console.log(profile.getTraverls());
+
+const travel = {
+  point: "Home",
+  destination: "Liverpool",
+  savedPlaces: ["Work", "Home", "Liverpool"],
+};
+
+console.log(":::::::::::::::: Uber - travel ::::::::::::::::");
+console.log(travel);
