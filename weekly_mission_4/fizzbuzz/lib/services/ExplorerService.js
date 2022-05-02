08da89bd8a242fb9dfe95f81388138db2277089c
Alfredo Bonell <alfredobonell96@gmail.com>
2022-05-01 18:46:55 -0600

    Se agregan las prácticas y se actualiza el readme.

---
+++ weekly_mission_4/fizzbuzz/lib/services/ExplorerService.js
@@ -0,0 +1,26 @@
+class ExplorerService {
+
+    static filterByMission(explorers, mission){
+        const explorersByMission = explorers.filter((explorer) => explorer.mission == mission);
+        return explorersByMission;
+    }
+
+    static getAmountOfExplorersByMission(explorers, mission){
+        const explorersByMission = ExplorerService.filterByMission(explorers, mission);
+        return explorersByMission.length;
+    }
+
+    static getExplorersUsernamesByMission(explorers, mission){
+        const explorersByMission = ExplorerService.filterByMission(explorers, mission);
+        const explorersUsernames = explorersByMission.map((explorer) => explorer.githubUsername);
+        return explorersUsernames;
+    }
+
+    static filterByTechology(explorers, technology){
+        const explorersByTechology = explorers.filter(explorer => explorer.stacks.includes(technology));
+        return explorersByTechology;
+    }
+
+}
+
+module.exports = ExplorerService;
